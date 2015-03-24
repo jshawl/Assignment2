132c900f966e1d3987f8d2ed7c69b518ea2f8370
Jesse Shawl <jesse@jshawl.com>
2015-03-24 14:54:39 -0400

    implement fourth view controller

--- src/Lesson02/FourthViewController.swift
+++ src/Lesson02/FourthViewController.swift
@@ -13,4 +13,18 @@ class FourthViewController: UIViewController {
     TODO seven: Hook up the text input box, label and and a ‘calculate’ button. Create a ‘fibonacci adder’ class with a method ‘fibonacciNumberAtIndex' which takes indexOfFibonacciNumber (an integer).  When the button is pressed, create an instance of that class, call the method, and print out the appropriate fibonacci number of an inputted integer.
     The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number.
 */
+    
+    
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var output: UILabel!
+    @IBAction func calculate(sender: AnyObject) {
+        var fib:[Int] = []
+        let stopAt = input.text.toInt()
+        fib[0] = 0
+        fib[1] = 1
+        for var i = 2; i < stopAt; i++ {
+          fib[i] = fib[i-2] + fib[i-1]
+        }
+        output.text = "\(fib[-1])"
+    }
 }
